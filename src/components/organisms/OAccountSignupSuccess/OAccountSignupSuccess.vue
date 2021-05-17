c4e03fb4285d4cbb6b039d734207591e5b21bb38
sakuragi1111 <sakuragi1157@gmail.com>
2021-05-18 08:59:14 +0900

    add signup page

--- src/components/organisms/OAccountSignupSuccess/OAccountSignupSuccess.vue
+++ src/components/organisms/OAccountSignupSuccess/OAccountSignupSuccess.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <button
-      @click="login"
-      class="w-full px-4 py-2 text-lg font-semibold text-white transition-colors duration-300 bg-gray-700 rounded-md shadow hover:bg-gray-800 focus:outline-none focus:ring-blue-200 focus:ring-4"
-    >
-      {{ $t("login") }}
-    </button>
+    <router-link :to="{ name: 'Login' }">
+      <button
+        class="w-full px-4 py-2 text-lg font-semibold text-white transition-colors duration-300 bg-gray-700 shadow hover:bg-gray-800 focus:outline-none"
+      >
+        {{ $t("login") }}
+      </button>
+    </router-link>
   </div>
 </template>
 
