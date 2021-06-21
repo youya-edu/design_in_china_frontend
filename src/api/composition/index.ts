da9fe4c3982a4a10db8ecb30a4d73488c092c909
sakuragi1111 <sakuragi1157@gmail.com>
2021-06-21 10:33:12 +0900

    feature: composition pagination

--- src/api/composition/index.ts
+++ src/api/composition/index.ts
@@ -1,16 +1,19 @@
-import { Composition } from "@/domain";
+import { CompositionCollection } from "@/domain";
 import { httpRequest } from "@/utils/http";
 import { API } from "./constant";
 
-const fetchCompositions = async (): Promise<Composition[]> => {
-  let compositions: Composition[] = [];
+const fetchCompositions = async (): Promise<CompositionCollection> => {
+  let compositionCollection: CompositionCollection = {
+    compostions: [],
+    totalSize: 0,
+  };
   try {
     const response = await httpRequest.get(API.COMPOSITIONS);
-    compositions = response.data;
+    compositionCollection = response.data;
   } catch (error) {
     console.error(error);
   }
-  return compositions;
+  return compositionCollection;
 };
 
 export { fetchCompositions };
